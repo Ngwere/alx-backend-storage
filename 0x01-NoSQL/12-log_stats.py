18967b53de9ef5d79d4c04f58062faa8f4828c36
Ngwere <ngwerecliford@gmail.com>
2024-08-29 02:28:05 +0000

    changed 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-'''Task 12's module.
-'''
+"""Module for task 12
+"""
 from pymongo import MongoClient
 
 
 def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
-    '''
+    """script that provides some stats about Nginx logs stored in MongoDB.
+
+    Args:
+        nginx_collection (pymongo.collection.Collection): collection to analyze
+    """
     print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
@@ -20,8 +23,8 @@ def print_nginx_request_logs(nginx_collection):
 
 
 def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
+    """Provides some stats about Nginx logs stored in MongoDB.
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
     print_nginx_request_logs(client.logs.nginx)
 
